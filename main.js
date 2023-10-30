3b6a52c6fc50a1750447b9fe702522862f1a950b
Ruturaj Kalshetti <ruturajk2404@gmail.com>
2023-10-30 09:04:00 +0530

    new-changes

--- main.js
+++ main.js
@@ -18,20 +18,34 @@
  * @property {number[]} vote
  *
  */
-
+/**
+ *
+ * @typedef {Object} MovieData
+ * @property {(number | undefined)} budget
+ * @property {string[]} genres
+ * @property {(string | undefined)} homepage
+ * @property {number} id
+ * @property {string} original_language
+ * @property {string} overview
+ * @property {number} popularity
+ * @property {string} poster_path
+ * @property {string} release_date
+ * @property {(number | undefined)} revenue
+ * @property {(number | undefined)} runtime
+ * @property {string} tagline
+ * @property {string} title
+ * @property {number} vote_average
+ * @property {number} vote_count
+ * @property {Object} external_ids
+ * @property {Object} similar
+ * @property {(string | undefined)} certification
+ * @property {Object} directors
+ * @property {Object} writers
+ * @property {Object} cast
+ * @property {(string | undefined)} trailer_yt
+ */
 /* ----- Type Definitions ----- */
-/* ----- HTML Computed Styles ----- */
-/* ----- HTML Computed Styles ----- */
 
-let mainApp = angular.module("main", []);
-
-mainApp.filter("fromMap", function () {
-  return function (input) {
-    var out = {};
-    input?.forEach((v, k) => (out[k] = v));
-    return out;
-  };
-});
 const toFixed = (number, precision) => {
   return parseFloat(number.toFixed(precision));
 };
@@ -70,163 +84,228 @@ const toDataSet = function (data) {
     return movies;
   }, new Map());
 };
+/**
+ *
+ * @param {Array<any>} _arr
+ * @returns {Array<any>}
+ */
+const shuffle = function (_arr) {
+  const arr = [..._arr].sort(() => 0.5 - Math.random());
+  return arr;
+};
 
-/**
- *
- * @param {Array<any>} data
- * @returns {Map<string, Movie[]}
- */
-
-const extractData = function (data) {
-  return data.reduce((movies, movie) => {
-    if (!movies.has(movie.original_language)) {
-      if (
-        movie.budget === undefined ||
-        movie.revenue === undefined ||
-        movie.vote_average === undefined
-      ) {
-        return movies;
-      }
-      movies.set(movie.original_language, [
-        {
-          budget: movie.budget,
-          revenue: movie.revenue,
-          vote: movie.vote_average,
-        },
-      ]);
-    } else {
-      if (
-        movie.budget === undefined ||
-        movie.revenue === undefined ||
-        movie.vote_average === undefined
-      ) {
-        return movies;
-      }
-      movies.get(movie.original_language).push({
-        budget: movie.budget,
-        revenue: movie.revenue,
-        vote: movie.vote_average,
-      });
-    }
-    return movies;
-  }, new Map());
-};
-/**
- *
- *  @param {Map<string, Movie[]>} data
- *  @returns {Map<string, number[][]}
- */
-const toArrMap = (data) => {
-  const m = new Map();
-  data.forEach((movies, lang) => {
-    m.set(
-      lang,
-      movies.reduce((acc, movie) => {
-        acc.push([movie.budget, movie.revenue, movie.vote]);
-        return acc;
-      }, [])
-    );
-  });
-  return m;
-};
-/**
- *
- * @param {Map<string, DataSet>}  data
- * @returns {Map<string, AvgMovie}
- */
-const getAvgData = (data) => {
-  const avgMap = new Map();
-  data.forEach((movie, lang) => {
-    if (
-      movie.budget.length < 10 ||
-      movie.revenue.length < 10 ||
-      movie.vote.length < 10
-    ) {
-      return;
-    }
-    const avg_budget = jStat(movie.budget).mean();
-    const avg_revenue = jStat(movie.revenue).mean();
-    const avg_vote = jStat(movie.vote).mean();
-    avgMap.set(lang, {
-      avg_budget: toFixed(avg_budget, 0),
-      avg_revenue: toFixed(avg_revenue, 0),
-      avg_vote: toFixed(avg_vote, 1),
-    });
-  });
-  return avgMap;
-};
-mainApp.controller("moviesController", function ($rootScope, $scope, $http) {
-  $rootScope.maxSelection = 3;
-  $rootScope.fields = [];
-  $rootScope.test = "chisquare";
-  $http.get("./data/tmdb_movies.json").then(
-    function (jsonData) {
-      const { data } = jsonData;
-      const dataSet = toDataSet(data);
-      $scope.avgMap = getAvgData(dataSet);
-      $rootScope.selected = [];
-      console.log($scope.avgMap);
-    },
-    function (err) {
-      console.log(err);
-    }
-  );
-  $scope.checkFields = (evt) => {
-    const input = $(evt.target);
-    if (input.is(":checked")) {
-      $rootScope.fields.push(input.val());
-    } else {
-      $rootScope.fields.splice($rootScope.fields.indexOf(input.val()), 1);
-    }
-    console.log($rootScope.fields);
-  };
-  $scope.handleSelect = (evt) => {
-    const input = $(evt.target);
-    const name = input.val();
-    if (
-      input.is(":checked") &&
-      $rootScope.selected.length < $rootScope.maxSelection
-    ) {
-      $rootScope.selected.push(name);
-    } else if (!input.is(":checked")) {
-      const idx = $rootScope.selected.indexOf(name);
-      $rootScope.selected.splice(idx, 1);
-    } else {
-      input.prop("checked", false);
-    }
-    console.log($rootScope.selected);
+console.log("Hello");
+let mainApp = angular.module("moviesApp", []);
+mainApp.filter("fromMap", function () {
+  return function (input) {
+    var out = {};
+    input?.forEach((v, k) => (out[k] = v));
+    return out;
   };
 });
 
+let MOVIE;
+const dont = [
+  "Animation",
+  "Western",
+  "Music",
+  "Thriller",
+  "TV Movie",
+  "War",
+  "Fantasy",
+  "Science Fiction",
+  "History",
+];
+
 mainApp.controller(
-  "statsController",
+  "moviesController",
   async function ($rootScope, $scope, $http) {
+    const formModal = new bootstrap.Modal("#data-test");
+    console.log(formModal);
+    formModal.show();
+    $rootScope.mode = "selection";
+
+    $rootScope.test = {
+      name: "",
+      what: "",
+      test_data: null,
+      avg_data: null,
+      selection: [],
+    };
+    $rootScope.constants = { ZCOUNT: 2 };
+    const allMovies = await $http.get("/data/tmdb_movies.json");
     /**
      * @constant
-     * @type {{movieMap: Map<string, DataSet>, avgMap: Map<string, AvgMovie>} | undefined}
+     * @type {{data: MovieData}}
      */
-
-    const data = await $http.get("./data/tmdb_movies.json").then(
-      function (jsonData) {
-        const { data } = jsonData;
-        const movieMap = toDataSet(data);
-        const avgMap = getAvgData(movieMap);
-
-        return { movieMap, avgMap };
-      },
-      function (err) {
-        console.log(err);
-        return;
+    const { data: movies } = allMovies;
+    MOVIE = movies;
+    const fields = new Set();
+    const allGenres = new Set();
+    console.log(movies);
+    const movieCounts = {};
+    movies.forEach((movie) => {
+      const { original_language, genres } = movie;
+      if (movieCounts[original_language] === undefined) {
+        movieCounts[original_language] = 1;
+      } else {
+        movieCounts[original_language] += 1;
       }
-    );
-    $rootScope.onCompare = function (evt) {
-      const { selected, test, fields } = $rootScope;
-      const testData = { data: {
-
-      } };
-      selected.forEach((seln) => {
-        testData.data[seln] = data.avgMap.get(seln);
+      if (typeof genres === "string") {
+        allGenres.add(genres);
+      } else {
+        genres.forEach((genre) => {
+          allGenres.add(genre);
+        });
+      }
+      Object.keys(movie).forEach((field) => {
+        fields.add(field);
       });
+    });
+
+    $scope.selection = [];
+    $scope.test_data = null;
+    $scope.avg_data = null;
+    $scope.isEnabled = false;
+    $rootScope.$watch("test.what", function () {
+      const { name, what } = $rootScope.test;
+      $scope.selection = [];
+      $scope.isEnabled = false;
+      if (name === "z_test" && what === "revenue") {
+        const langs = Object.keys(movieCounts).filter(
+          (k) => movieCounts[k] >= 100
+        );
+        const revenues = langs.reduce((acc, lang) => {
+          acc[lang] = [];
+          movies.forEach(({ original_language, revenue }) => {
+            if (original_language === lang && revenue !== undefined) {
+              acc[lang].push(revenue);
+            }
+          });
+          return acc;
+        }, new Object());
+        $scope.test_data = revenues;
+        $scope.avg_data = Object.entries(revenues).reduce(
+          (avgAcc, [lang, revenue]) => {
+            avgAcc[lang] = jStat(revenue).mean();
+            return avgAcc;
+          },
+          new Object()
+        );
+      }
+      if (name === "z_test" && what === "genre") {
+        let genPop = {};
+        allGenres.forEach((genre) => {
+          genPop[genre] = [];
+          movies.forEach(({ genres, popularity }) => {
+            if (genres && genres[0] === genre && popularity !== undefined) {
+              genPop[genre].push(popularity);
+            }
+          });
+        });
+        console.log(genPop);
+        genPop = Object.entries(genPop)
+          .filter(([k, v]) => v.length >= 100)
+          .reduce((acc, [k, v]) => {
+            acc[k] = v;
+            return acc;
+          }, new Object());
+        $scope.test_data = genPop;
+        $scope.avg_data = Object.entries(genPop).reduce((avgAcc, [k, v]) => {
+          avgAcc[k] = jStat(v).mean();
+          return avgAcc;
+        }, new Object());
+      }
+    });
+
+    $(document).on("click", ".lang-card, .genre-card", function (evt) {
+      const el = $(evt.currentTarget);
+      const attr = el.data("st-lang") || el.data("st-genre");
+      console.log(attr);
+      if (
+        !$scope.selection.includes(attr) &&
+        $scope.selection.length < $rootScope.constants.ZCOUNT
+      ) {
+        el.addClass("selected");
+        $scope.$apply(function () {
+          $scope.selection.push(attr);
+        });
+      } else {
+        el.removeClass("selected");
+        const idx = $scope.selection.indexOf(attr);
+        if (idx !== -1) {
+          $scope.$apply(function () {
+            $scope.selection.splice(idx, 1);
+          });
+        }
+      }
+      $scope.$apply(function () {
+        $scope.isEnabled = $scope.selection.length === 2;
+      });
+    });
+
+    $scope.moveToRoot = function () {
+      $rootScope.test.test_data = $scope.test_data;
+      $rootScope.test.avg_data = $scope.avg_data;
+      $rootScope.test.selection = $scope.selection;
+      $rootScope.mode = "test";
+      formModal.hide();
     };
   }
 );
+mainApp.controller("testController", function ($rootScope, $scope) {
+  /**
+   * @constant
+   * @type {{ test_data: Object, avg_data: Object, what: string, name: string, selection: string[] }}
+   */
+  const { test_data, avg_data, what, name, selection } = $rootScope.test;
+
+  $scope.selection = selection;
+  $scope.tested = false;
+  $scope.tables = {
+    z_test(selection, test_data) {
+      const samples = selection.reduce((_acc, sel) => {
+        const _newArr = shuffle(test_data[sel]);
+        _acc[sel] = _newArr.slice(0, Math.round(_newArr.length / 2));
+        return _acc;
+      }, {});
+      const sampMeans = selection.reduce((_acc, sel) => {
+        _acc[sel] = jStat(samples[sel]).mean();
+        return _acc;
+      }, new Object());
+      $scope.means = [sampMeans[selection[0]], sampMeans[selection[1]]];
+
+      const stdDevs = selection.reduce((_acc, sel) => {
+        _acc[sel] = jStat(test_data[sel]).stdev();
+        return _acc;
+      }, new Object());
+      return [sampMeans, stdDevs];
+    },
+  };
+  $scope._testFns = {
+    z_test(sampMeans, popStDev, selection) {
+      const meanDiff = sampMeans[selection[0]] - sampMeans[selection[1]];
+      const popVariance =
+        Math.pow(popStDev[selection[0]], 2) / test_data[selection[0]].length +
+        Math.pow(popStDev[selection[1]], 2) / test_data[selection[1]].length;
+      const z_score = meanDiff / Math.sqrt(popVariance);
+      return jStat.ztest(z_score, 2);
+    },
+  };
+  $scope.hypothesis = function () {
+    if($scope.tested){
+      return;
+    }
+    const [sampMeans, popStDev] = $scope.tables[name](selection, test_data);
+    $scope.means = sampMeans;
+    $scope.stdevs = popStDev;
+    $scope.what = what;
+    $scope.name = name;
+  };
+  $scope.test = function () {
+    $scope.pval = $scope._testFns[name]($scope.means, $scope.stdevs, selection);
+    console.log($scope.pval)
+    $scope.tested = true
+  };
+});
+// "genres":\s?"(\w+)"
